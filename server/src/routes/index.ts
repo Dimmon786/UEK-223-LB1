ba9ac47db7d0f2d175e9da8e9e320367b9775801
Dimmon786 <bezborodko.dmytro@gmail.com>
2024-04-12 10:25:43 +0200

    frontend

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,16 @@
-import { Application } from "express";
+import express, { Application } from "express";
 import usersRoutes from "./users.routes";
 import roleRoutes from "./role.routes";
 import postRoutes from "./post.routes";
 import likeRoutes from "./like.routes";
 import commentRoutes from "./comment.routes";
 import homeRoutes from "./home.routes";
+import path from 'path';
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 export default class Routes {
   constructor(app: Application) {
@@ -13,9 +19,12 @@ export default class Routes {
     app.use("/role", roleRoutes);
     app.use("/like", likeRoutes);
     app.use("/comment", commentRoutes);
+    app.use(express.static(path.join(__dirname, '../../public')));
 
-    app.use("/", (req, res) => {
-      res.json({ message: "Welcome to the application." });
+    app.get('*', (req, res) => {
+      // Adjust the path based on where the index.html is located relative to this script
+      res.sendFile(path.resolve(__dirname, '../../public', 'index.html'));
     });
+
   }
 }
